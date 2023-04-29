f9aaf2227018cf38989cc7da2b81e5cdf4902fde
Spenser Laier <ssl510@nyu.edu>
2023-04-29 15:00:33 -0400

    redirect to home without auth; changed naming

--- resources/views/layouts/app-master.blade.php
+++ resources/views/layouts/app-master.blade.php
@@ -6,7 +6,7 @@
     <meta name="description" content="">
     <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
     <meta name="generator" content="Hugo 0.87.0">
-    <title>Ballooning Nest Eggs Assessment</title>
+    <title>BNE Assessment</title>
 
     <!-- Bootstrap core CSS -->
     <link href="{!! url('assets/bootstrap/css/bootstrap.min.css') !!}" rel="stylesheet">
